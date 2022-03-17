439d244310908237511faa7bff9d795995e2d013
DGomez1122 <dgomez.dg323@gmail.com>
2022-03-16 19:18:44 -0600

    Añadiendo interfaz

---
+++ app/src/main/java/com/example/tecbank/pantallacuentas.java
@@ -0,0 +1,14 @@
+package com.example.tecbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pantallacuentas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantallacuentas);
+    }
+}
