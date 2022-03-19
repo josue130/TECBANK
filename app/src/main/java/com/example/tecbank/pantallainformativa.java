944a4509c0fa26a9bfbbbe9e8f1460467d4e4675
DGomez1122 <dgomez.dg323@gmail.com>
2022-03-19 11:34:46 -0600

    Actualización Interfaz

---
+++ app/src/main/java/com/example/tecbank/pantallainformativa.java
@@ -0,0 +1,14 @@
+package com.example.tecbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pantallainformativa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantallainformativa);
+    }
+}
