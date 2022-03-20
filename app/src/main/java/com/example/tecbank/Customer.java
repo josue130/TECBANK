a11fc9ba0b936ab6675f191d2404997a12931bba
jesus-cl27 <jcruz2711@outlook.es>
2022-03-19 18:30:00 -0600

    Clase customer

---
+++ app/src/main/java/com/example/tecbank/Customer.java
@@ -0,0 +1,58 @@
+package com.example.tecbank;
+
+public class Customer {
+    private  int id;
+    private  String nombre;
+    private  String correo;
+    private  String cuenta;
+    private  String contrasenna;
+    private int  monto;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public String getContrasenna() {
+        return contrasenna;
+    }
+
+    public void setContrasenna(String contrasenna) {
+        this.contrasenna = contrasenna;
+    }
+
+    public int getMonto() {
+        return monto;
+    }
+
+    public void setMonto(int monto) {
+        this.monto = monto;
+    }
+}
